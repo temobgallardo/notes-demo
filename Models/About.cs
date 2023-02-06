f9b25c7a290d42afc4cefd6f768bc1872640e924
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-02-06 15:43:31 -0500

    removed static word

--- Models/About.cs
+++ Models/About.cs
@@ -1,12 +1,12 @@
 ﻿namespace FlyoutPageDemoMaui.Models;
 
-public static class About
+public class About
 {
-  public static string Title => AppInfo.Name;
+  public string Title => AppInfo.Name;
 
-  public static string Version => AppInfo.VersionString;
+  public string Version => AppInfo.VersionString;
 
-  public static string MoreInfoUrl => "https://aka.ms/maui";
+  public string MoreInfoUrl => "https://aka.ms/maui";
 
-  public static string Message => "This app is written in XAML and C# with .NET MAUI.";
+  public string Message => "This app is written in XAML and C# with .NET MAUI.";
 }
