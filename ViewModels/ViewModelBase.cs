68808679fcc4907b7cd66f4a8932b223a5d404fe
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-02-07 10:18:22 -0500

    added bindablebase and viewmodelbase

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿namespace FlyoutPageDemoMaui.ViewModels;
+
+public class ViewModelBase : BindableBase, IDisposable
+{
+  private bool disposedValue;
+
+  protected virtual void Dispose(bool disposing)
+  {
+    if (!disposedValue)
+    {
+      if (disposing)
+      {
+        // TODO: dispose managed state (managed objects)
+      }
+
+      // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+      // TODO: set large fields to null
+      disposedValue = true;
+    }
+  }
+
+  // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+  // ~ViewModelBase()
+  // {
+  //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+  //     Dispose(disposing: false);
+  // }
+
+  public void Dispose()
+  {
+    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
+}
