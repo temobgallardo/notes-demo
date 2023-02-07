a2ac5cc9756597c5663ff7ea7a0c5dcaffd1a921
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-02-07 10:21:18 -0500

    used the binded model

--- Views/AboutPage.xaml.cs
+++ Views/AboutPage.xaml.cs
@@ -2,14 +2,17 @@ namespace FlyoutPageDemoMaui.Views;
 
 public partial class AboutPage : ContentPage
 {
-	public AboutPage()
-	{
-		InitializeComponent();
-	}
-    
+  public AboutPage()
+  {
+    InitializeComponent();
+  }
+
   private async void LearnMore_Clicked(object sender, EventArgs e)
   {
-    // Navigate to the specified URL in the system browser.
-    await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    if (sender is Models.About about)
+    {
+      // Navigate to the specified URL in the system browser.
+      await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+    }
   }
 }
