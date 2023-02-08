db6000594191aaf120966d74844d43b1875cc522
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-02-08 14:16:06 -0500

    made NoteViewModel the binding context of the NotePage and binded the properties and commands

--- Views/NotePage.xaml.cs
+++ Views/NotePage.xaml.cs
@@ -2,31 +2,10 @@ namespace FlyoutPageDemoMaui.Views;
 
 public partial class NotePage : ContentPage
 {
-  public const string DefaultFileName = "notes.txt";
-  readonly string _fileName = Path.Combine(FileSystem.AppDataDirectory, DefaultFileName);
-
   public NotePage()
   {
     InitializeComponent();
 
-    if (File.Exists(_fileName))
-    {
-      TextEditor.Text = File.ReadAllText(_fileName);
-    }
-  }
-
-  private void SaveButton_Clicked(object sender, EventArgs e)
-  {
-    File.WriteAllText(_fileName, TextEditor.Text);
-  }
-
-  private void DeleteButton_Clicked(object sender, EventArgs e)
-  {
-    if (File.Exists(_fileName))
-    {
-      File.Delete(_fileName);
-    }
-
-    TextEditor.Text = string.Empty;
+    BindingContext = new ViewModels.NoteViewModel();
   }
 }
