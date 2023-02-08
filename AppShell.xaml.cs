00fd203cab3f50ef39d9d57924827b46cf1f042c
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-02-08 12:11:25 -0500

    added NotePage to global routes

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace FlyoutPageDemoMaui;
+﻿using FlyoutPageDemoMaui.Views;
+
+namespace FlyoutPageDemoMaui;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(NotePage), typeof(NotePage));
 	}
 }
