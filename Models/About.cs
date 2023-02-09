40e10119ca991c84b0b00d4d4754a09781def9a3
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-02-09 11:52:26 -0500

    added more text to message given the intention of this project

--- Models/About.cs
+++ Models/About.cs
@@ -8,5 +8,5 @@ public class About
 
   public string MoreInfoUrl => "https://aka.ms/maui";
 
-  public string Message => "This app is written in XAML and C# with .NET MAUI.";
+  public string Message => "This app is written in XAML and C# with .NET MAUI. \nIt is intended for POC creation with multiple tools";
 }
