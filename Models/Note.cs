939e20c7ec1bfeaf7cf2eefc2e3f9e08473e1f36
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-03-07 16:30:17 -0500

    added LastUpdatedDate property

--- Models/Note.cs
+++ Models/Note.cs
@@ -7,6 +7,7 @@ public class Note : BindableBase
   private string _fileName;
   private string _text;
   private DateTime _date;
+  private DateTime _lastUpdatedDate;
 
   public int Id { get; set; }
 
@@ -15,4 +16,6 @@ public class Note : BindableBase
   public string Text { get => _text; set => SetProperty(ref _text, value); }
 
   public DateTime Date { get => _date; set => SetProperty(ref _date, value); }
+
+  public DateTime LastUpdatedDate { get => _lastUpdatedDate; set => SetProperty(ref _lastUpdatedDate, value); }
 }
