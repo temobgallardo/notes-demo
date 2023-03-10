bd6ce76d25222030f99fdd12256f0cb234aac3c8
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-03-10 13:31:53 -0500

    added observablecollection extensions

---
+++ Extensions/ObservableCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FlyoutPageDemoMaui.Extensions;
+
+public static class ObservableCollectionExtension
+{
+  public static void AddRange<T>(this ObservableCollection<T> self, IEnumerable<T> data) where T : class
+  {
+    foreach (var item in data)
+    {
+      self.Add(item);
+    }
+  }
+
+  public static void ClearAndAddRange<T>(this ObservableCollection<T> self, IEnumerable<T> data) where T : class
+  {
+    self.Clear();
+    foreach (var item in data)
+    {
+      self.Add(item);
+    }
+  }
+}
