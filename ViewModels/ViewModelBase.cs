7cc3beb248985474526117571d4f22f3af5a1062
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-03-10 14:00:59 -0500

    added base methods to execute async operations and handle errors. Also injected the repository and logger

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,17 +1,51 @@
-﻿
+﻿using FlyoutPageDemoMaui.Offline;
+using Microsoft.Extensions.Logging;
 
 namespace FlyoutPageDemoMaui.ViewModels;
 
 public abstract class ViewModelBase : BindableBase, IDisposable
 {
 
-  protected ViewModelBase()
+  protected ViewModelBase(INoteRepository repository, ILogger logger)
   {
-
+    Repository = repository;
+    Logger = logger;
   }
+
+  public INoteRepository Repository { get; }
+  public ILogger Logger { get; }
+
 
   // TODO: Inject MainThread for Unit Testing
   protected static void OnMainThread(Func<Task> function) => MainThread.BeginInvokeOnMainThread(async () => await function());
+
+  protected void ExecAndHandleException(Action function)
+  {
+    try
+    {
+      function();
+    }
+    catch (Exception ex)
+    {
+      Logger.LogError(ex, "***Something wrong happened***");
+    }
+  }
+
+  protected async Task<bool> ExecAndHandleExceptionAsync(Func<Task> function)
+  {
+    bool success = false;
+    try
+    {
+      await function();
+      success = true;
+    }
+    catch (Exception ex)
+    {
+      Logger.LogError(ex, "***Something wrong happened***");
+    }
+
+    return success;
+  }
 
   #region Dispose
   private bool disposedValue;
