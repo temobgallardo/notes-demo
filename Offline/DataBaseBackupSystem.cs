be9a3765205646b4e0c60f16c0b4475f257adfa2
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-03-13 13:05:20 -0400

    added functionality to create a copy of data files of litedb db

---
+++ Offline/DataBaseBackupSystem.cs
@@ -0,0 +1,22 @@
+﻿namespace FlyoutPageDemoMaui.Offline;
+
+public static class DatabaseBackupSystem
+{
+  public static async Task StartBackup(string dbLocation)
+  {
+    var directory = Directory.GetParent(dbLocation);
+
+    //var time = DateTime.Now.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+    var backupPath = Path.Combine(directory.FullName, $"-backup.odb");
+
+    //var db = Locator.Current.GetService<AppDbManager>();
+    //await db.DoInterruptedOperationAsync(async () =>
+    //{
+    //  await Observable.Start(() =>
+    //  {
+    //    File.Copy(dbLocation, backupPath);
+    //  });
+    //});
+    File.Copy(dbLocation, backupPath);
+  }
+}
