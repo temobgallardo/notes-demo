e2a1d552a49da1465a05d522bbd6ec9fd4fcc00b
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2023-03-30 10:33:55 -0400

    added view model to about (facing issue with navigation)

---
+++ ViewModels/AboutViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using FlyoutPageDemoMaui.Models;
+using FlyoutPageDemoMaui.Offline;
+using Microsoft.Extensions.Logging;
+
+namespace FlyoutPageDemoMaui.ViewModels;
+
+public class AboutViewModel : ViewModelBase<About>
+{
+  public AboutViewModel(INoteRepository repository, ILogger logger, IRepositoryManager repositoryManager) : base(repository, logger)
+  {
+    RepositoryManager = repositoryManager;
+  }
+
+  public ICommand OpenAboutCommand => new Command(async () => await Launcher.Default.OpenAsync(Model.MoreInfoUrl));
+
+  public ICommand OpenMetroLogs => new Command(() => Model.GoToLogsPageCommand?.Execute(null));
+
+  public ICommand BackupAndRebuildCommand => new Command(async () => await BackupAndRebuild());
+
+  public IRepositoryManager RepositoryManager { get; }
+
+  private async Task BackupAndRebuild()
+  {
+    await RepositoryManager.BackupAndRebuild();
+  }
+}
--- Views/AboutPage.xaml.cs
+++ Views/AboutPage.xaml.cs
@@ -1,31 +1,15 @@
 using FlyoutPageDemoMaui.Models;
+using FlyoutPageDemoMaui.ViewModels;
 using MetroLog.Maui;
 
 namespace FlyoutPageDemoMaui.Views;
 
 public partial class AboutPage : ContentPage
 {
-  public AboutPage()
+  public AboutPage(ViewModelBase<About> vm)
   {
     InitializeComponent();
 
-    BindingContext = new About();
-  }
-
-  private async void LearnMore_Clicked(object sender, EventArgs e)
-  {
-    if (sender is Models.About about)
-    {
-      // Navigate to the specified URL in the system browser.
-      await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-    }
-  }
-
-  private void Metrologs_Clicked(object sender, EventArgs e)
-  {
-    var logController = (LogController)sender;
-
-    // will show the MetroLogPage by default
-    logController.GoToLogsPageCommand?.Execute(null);
+    BindingContext = vm;
   }
 }
